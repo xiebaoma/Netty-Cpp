e2fb6051faebe05eb159ce69b0e9eaf22745ed46
xiebaoma <2402583447@qq.com>
2025-06-14 23:04:26 +0800

    reinit the project

--- MingX/Source/DstVersion.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define Flamingo_MAJOR_VERSION    1
-#define Flamingo_MINOR_VERSION    0
-#define Flamingo_BUILD_VERSION    1526
-
-#define Flamingo_VERSION "1.0.0.1526"
--- MingX/Source/Time.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-//保存聊天图片的文件名
-void GenerateChatImageSavedName(CString& strFileName);
