a9a7ed0e1a0f5e26fe32c94389df8a38ac0335cb
xiebaoma <2402583447@qq.com>
2025-06-15 19:05:15 +0800

    code the template class for singleton mode and code the test

---
+++ base/Singleton.h
@@ -0,0 +1,24 @@
+/*
+ *  Filename:   Singleton.h
+ *  Author:     xiebaoma
+ *  Date:       2025-06-15
+ *  Description:Template class for singleton mode
+ */
+
+#pragma once
+
+template <typename T>
+class Singleton
+{
+public:
+    static T &Instance()
+    {
+        static T instance;
+        return instance;
+    }
+
+    Singleton() = delete;
+    ~Singleton() = delete;
+    Singleton(const Singleton &) = delete;
+    Singleton &operator=(const Singleton &) = delete;
+};
---
+++ test/SingletonTest.cpp
@@ -0,0 +1,32 @@
+/*
+ *  Filename:   SingletonTest.cpp
+ *  Author:     xiebaoma
+ *  Date:       2025-06-15
+ *  Description:test singleton mode
+ */
+
+#include <iostream>
+
+#include "../base/Singleton.h"
+
+class mytype
+{
+public:
+    int a = 10;
+};
+
+int main()
+{
+    mytype &a = Singleton<mytype>::Instance();
+    mytype &b = Singleton<mytype>::Instance();
+
+    if (&a == &b)
+    {
+        std::cout << "a and b point to the same param";
+    }
+    else
+    {
+        std::cout << "a and b point to the different param";
+    }
+    return 0;
+}
